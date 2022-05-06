702ce3072b5a06c9449ef89f6c1521cf889cefb6
santosraquel <raquel.dossantos1965@gmail.com>
2022-05-06 20:42:55 -0300

    criando algumas classes do projeto

--- psicoclin/paginas/admin.py
+++ psicoclin/paginas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Estado, Cidade, Pessoa
+
+admin.site.register(Estado)
+admin.site.register(Cidade)
+admin.site.register(Pessoa)
--- psicoclin/paginas/models.py
+++ psicoclin/paginas/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Estado(models.Model):
+    sigla = models.CharField(max_length=2)
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.sigla
+
+
+class Cidade(models.Model):
+    nome = models.CharField(max_length=50)
+    estado = models.ForeignKey(Estado, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.nome + "/" + self.estado
+
+
+class Pessoa(models.Model):
+
+    nome = models.CharField(
+        max_length=50, verbose_name="Nome", help_text="Digite seu nome completo")
+    data_nascimento = models.DateField(verbose_name='Data de Nascimento')
+    email = models.CharField(max_length=100)
+
+    cidade = models.ForeignKey(Cidade, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return '{} ({})'.format(self.nome, self.data_nascimento)
