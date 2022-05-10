3b542613271773d39eee180f729e8f38b7f23870
santosraquel <raquel.dossantos1965@gmail.com>
2022-05-09 21:06:20 -0300

    criando views

--- psicoclin/paginas/urls.py
+++ psicoclin/paginas/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from .views import Index
+
+from .views import Index, EstadoCreate, CidadeCreate, PessoaCreate
+from .views import EstadoUpdate, CidadeUpdate, PessoaUpdate
+
 
 urlpatterns = [
     path('', Index.as_view(), name="index"),
+
+    path('cadastrar/estado', EstadoCreate.as_view(), name="cadastrar-estado"),
+    path('cadastrar/cidade', CidadeCreate.as_view(), name="cadastrar-cidade"),
+    path('cadastrar/pessoa', PessoaCreate.as_view(), name="cadastrar-pessoa"),
+
+    path('editar/estado/<int:pk>/', EstadoUpdate.as_view(), name="editar-estado"),
+    path('editar/cidade/<int:pk>/', CidadeUpdate.as_view(), name="editar-cidade"),
+    path('editar/pessoa/<int:pk>/', PessoaUpdate.as_view(), name="editar-pessoa"),
 ]
--- psicoclin/paginas/views.py
+++ psicoclin/paginas/views.py
@@ -1,6 +1,51 @@
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView, UpdateView
+
+from .models import Estado, Cidade, Pessoa
 # Create your views here.
 
 
+############################# CREATE VIEW #########################
 class Index(TemplateView):
     template_name = 'paginas/index.html'
+
+class EstadoCreate(CreateView):
+    model = Estado
+    fields = ['nome', 'sigla']
+    template_name = 'paginas/form.html'
+    sucess_url = reverse_lazy('index')
+
+class CidadeCreate(CreateView):
+    model = Cidade
+    fields = ['nome', 'estado']
+    template_name = 'paginas/form.html'
+    sucess_url = reverse_lazy('index')
+
+class PessoaCreate(CreateView):
+    model = Pessoa
+    fields = ['nome', 'data_nascimento', 'email', 'cidade']
+    template_name = 'paginas/form.html'
+    sucess_url = reverse_lazy('index')
+
+
+############################# UPDATE VIEW #########################
+
+class EstadoUpdate(UpdateView):
+    model = Estado
+    fields = ['nome', 'sigla']
+    template_name = 'paginas/form.html'
+    sucess_url = reverse_lazy('index')
+
+
+class CidadeUpdate(UpdateView):
+    model = Cidade
+    fields = ['nome', 'estado']
+    template_name = 'paginas/form.html'
+    sucess_url = reverse_lazy('index')
+
+
+class PessoaUpdate(UpdateView):
+    model = Pessoa
+    fields = ['nome', 'data_nascimento', 'email', 'cidade']
+    template_name = 'paginas/form.html'
+    sucess_url = reverse_lazy('index')
