e3aaf1bb790d830b074fff7ea9dae1988bdb4c46
santosraquel <raquel.dossantos1965@gmail.com>
2022-06-13 20:46:17 -0300

    mostrando menu superior

--- psicoclin/usuarios/urls.py
+++ psicoclin/usuarios/urls.py
@@ -1,10 +1,13 @@
+from re import template
 from django.urls import path
 from django.contrib.auth import views
 
 urlpatterns = [
+
     path('entrar/', views.LoginView.as_view(
-        template_name = 'usuarios/login.html'
+        template_name='usuarios/form.html'
     ), name='login'),
 
     path('sair/', views.LogoutView.as_view(), name='logout'),
+
 ]
